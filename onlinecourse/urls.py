30194d6e2d2d7a3f9dff848899656a53bccfb348
krPiyush2816 <krpiyush2828@gmail.com>
2025-12-25 20:25:34 +0530

    Update comments for exam submission paths

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 app_name = 'onlinecourse'
 urlpatterns = [
-    # Path for exam submission
+    # Path for exam submission logic
     path('course/<int:course_id>/submit/', views.submit, name='submit'),
     
-    # Path for showing exam result
+    # Path for displaying the final exam result
     path('course/<int:course_id>/submission/<int:submission_id>/result/', 
          views.show_exam_result, name='show_exam_result'),
 ]
